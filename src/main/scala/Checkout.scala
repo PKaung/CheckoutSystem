c31fc3231c09c7bf571676a480974f3b1a1026b7
paingkaung <paing.kaung@gmail.com>
2024-09-10 14:07:51 +0100

    Test (pass): Total cost of an apple and orange should be £0.85

--- src/main/scala/Checkout.scala
+++ src/main/scala/Checkout.scala
@@ -1,5 +1,6 @@
 class Checkout {
   def totalCost(items: List[String]): BigDecimal = {
-    BigDecimal(0.00)
+    val prices = Map("Apple" -> BigDecimal(0.60), "Orange" -> BigDecimal(0.25))
+    items.map(prices).sum.setScale(2,BigDecimal.RoundingMode.HALF_UP)
   }
 }
